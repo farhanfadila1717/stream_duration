e7144ac0c5170c28fc2216cfa357fddc7a1862bd
unknown <farhan.fadila1717@gmail.com>
2021-07-21 07:51:11 +0700

    Remove local Stream<Duration>

--- lib/src/stream_duration_base.dart
+++ lib/src/stream_duration_base.dart
@@ -4,16 +4,16 @@ class StreamDuration {
   late final StreamController<Duration> _streamController =
       StreamController<Duration>();
   Stream<Duration> get durationLeft => _streamController.stream;
-  late Stream<Duration> _getDurationLeft;
   StreamSubscription<Duration>? _streamSubscription;
 
   StreamDuration(Duration duration, Function onDone) {
     try {
       var _durationLeft = duration;
-      _getDurationLeft = Stream<Duration>.periodic(Duration(seconds: 1), (_) {
+
+      _streamSubscription =
+          Stream<Duration>.periodic(Duration(seconds: 1), (_) {
         return _durationLeft -= Duration(seconds: 1);
-      });
-      _streamSubscription = _getDurationLeft.listen((event) {
+      }).listen((event) {
         if (!_streamController.isClosed) {
           _streamController.add(event);
         }
--- test/sream_duration_test.dart
+++ test/sream_duration_test.dart
@@ -1,1 +1,23 @@
-void main() {}
+import 'package:stream_duration/stream_duration.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Test stream duration', () {
+    late StreamDuration streamDuration;
+
+    var expectOutput = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+    var index = 0;
+
+    setUp(() {
+      streamDuration = StreamDuration(Duration(seconds: 10), () {
+        print('Stream Donde & disposed');
+      });
+    });
+    test('Return Stream matcher dummyOutput', () {
+      streamDuration.durationLeft.listen(expectAsync1((event) {
+        expect(event.inSeconds, expectOutput[index]);
+        index++;
+      }));
+    });
+  });
+}
