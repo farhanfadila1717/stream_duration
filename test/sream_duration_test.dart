474d8c39be93d0b6b80056b2ce280651bc47aa44
Farhan Mac <farhan.fadila1717@gmail.com>
2022-01-12 15:48:57 +0700

    update test

--- test/sream_duration_test.dart
+++ test/sream_duration_test.dart
@@ -2,22 +2,37 @@ import 'package:stream_duration/stream_duration.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Test stream duration', () {
-    late StreamDuration streamDuration;
+  group(
+    'Test stream duration\n',
+    () {
+      late StreamDuration streamDuration;
 
-    var expectOutput = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
-    var index = 0;
+      var expectOutput = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+      var index = 0;
 
-    setUp(() {
-      streamDuration = StreamDuration(Duration(seconds: 10), onDone: () {
-        print('Stream Donde & disposed');
-      });
-    });
-    test('Return Stream matcher dummyOutput', () {
-      streamDuration.durationLeft.listen(expectAsync1((event) {
-        expect(event.inSeconds, expectOutput[index]);
-        index++;
-      }));
-    });
-  });
+      setUp(
+        () {
+          streamDuration = StreamDuration(
+            Duration(seconds: 10),
+            onDone: () {
+              print('Stream Donde & disposed');
+            },
+          );
+        },
+      );
+      test(
+        'Return Stream match',
+        () {
+          streamDuration.durationLeft.listen(
+            expectAsync1(
+              (event) {
+                expect(event.inSeconds, expectOutput[index]);
+                index++;
+              },
+            ),
+          );
+        },
+      );
+    },
+  );
 }
