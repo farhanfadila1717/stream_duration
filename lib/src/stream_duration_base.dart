c60df178e7d0a2c46ed902090e5c10f171d46569
Farhan Mac <farhan.fadila1717@gmail.com>
2022-01-20 09:50:14 +0700

    Add play, pause, resume

--- lib/src/stream_duration_base.dart
+++ lib/src/stream_duration_base.dart
@@ -1,75 +1,100 @@
 import 'dart:async';
 
+extension DurationExtension on Duration {
+  bool isSameDuration(Duration b) {
+    return inSeconds == b.inSeconds;
+  }
+}
+
 class StreamDuration {
-  late final StreamController<Duration> _streamController =
+  final StreamController<Duration> _streamController =
       StreamController<Duration>();
+  late Duration _durationLeft;
+  bool isPlaying = false;
   Stream<Duration> get durationLeft => _streamController.stream;
   StreamSubscription<Duration>? _streamSubscription;
 
+  final Duration duration;
+  final Function? onDone;
+  final bool countUp;
+  final bool infinity;
+  final bool autoPlay;
+
   StreamDuration(
-    Duration duration, {
-    Function? onDone,
-    bool countUp = false,
-    bool infinity = false,
+    this.duration, {
+    this.countUp = false,
+    this.infinity = false,
+    this.onDone,
+    this.autoPlay = true,
   }) {
-    try {
-      var _durationLeft = countUp ? Duration.zero : duration;
-      if (countUp) {
-        _durationLeft += Duration(seconds: 1);
-      } else {
-        _durationLeft -= Duration(seconds: 1);
-      }
-      if (!_streamController.isClosed) {
-        _streamController.add(_durationLeft);
-      }
-      _streamSubscription =
-          Stream<Duration>.periodic(Duration(seconds: 1), (_) {
+    _durationLeft = duration;
+    if (autoPlay) {
+      play();
+      isPlaying = true;
+    }
+  }
+
+  void play() {
+    if (_streamController.hasListener) return;
+    print('Play');
+    if (countUp) {
+      _durationLeft += Duration(seconds: 1);
+    } else {
+      _durationLeft -= Duration(seconds: 1);
+    }
+    if (!_streamController.isClosed) {
+      _streamController.add(_durationLeft);
+    }
+
+    _streamSubscription = Stream<Duration>.periodic(Duration(seconds: 1), (_) {
+      if (!(_streamSubscription?.isPaused ?? true)) {
         if (countUp) {
           return _durationLeft += Duration(seconds: 1);
         } else {
           return _durationLeft -= Duration(seconds: 1);
         }
-      }).listen(
-        (event) {
-          if (_streamController.isClosed) return;
-          _streamController.add(event);
+      }
+      return Duration.zero;
+    }).listen(
+      (event) {
+        if (_streamController.isClosed) return;
+        _streamController.add(_durationLeft);
 
-          if (countUp) {
-            if (!infinity) {
-              if (event.isSameDuration(duration)) {
-                dispose();
-                Future.delayed(Duration(seconds: 1), () {
-                  if (onDone != null) {
-                    onDone();
-                  }
-                });
-              }
-            }
-          } else {
-            if (event.inSeconds == 0) {
+        if (countUp) {
+          if (!infinity) {
+            if (_durationLeft.isSameDuration(duration)) {
               dispose();
               Future.delayed(Duration(seconds: 1), () {
                 if (onDone != null) {
-                  onDone();
+                  onDone!();
                 }
               });
             }
           }
-        },
-      );
-    } catch (e) {
-      throw 'Stream Duration: error' + e.toString();
-    }
+        } else {
+          if (_durationLeft.inSeconds == 0) {
+            dispose();
+            Future.delayed(Duration(seconds: 1), () {
+              if (onDone != null) {
+                onDone!();
+              }
+            });
+          }
+        }
+      },
+    );
+  }
+
+  void pause() {
+    _streamSubscription?.pause();
+  }
+
+  void resume() {
+    _streamSubscription?.resume();
   }
 
   void dispose() {
     _streamSubscription?.cancel();
     _streamController.close();
-  }
-}
-
-extension DurationExtension on Duration {
-  bool isSameDuration(Duration b) {
-    return inSeconds == b.inSeconds;
   }
 }
